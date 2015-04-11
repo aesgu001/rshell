da002b83ac9a6c8fe3eb3f8f001a1681ecfe8be9
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-10 20:39:41 -0700

    added while loop and parse function for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <unistd.h>
+#include <queue>
 #include "cmd.h"
 using namespace std;
 
+// also determines the number of commands to enqueue
+void parse(string line) {}
+
 int main(int argc, char **argv) {
-    cmd command = cmd();
+    queue<cmd> commands;
+    string line;
+    while (commands.empty()||commands.front().get_exec()!="exit") {
+        cout << "$ ";
+        cin >> line;
+        parse(line);
+    }
     return 0;
 }
