9af04e078909cfda2947eebda96e6650d972da5a
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-10 23:01:14 -0700

    cleared both main.cpp and cmd.h

--- src/cmd.h
+++ src/cmd.h
@@ -1,34 +1,9 @@
 #ifndef CMD_H
 #define CMD_H
 
-#include <vector>
-
-//struct argumentList {
-//    argumentList() {}
-//    argumentList(std::string arg) : argument(arg) {}
-//    argumentList *next;
-//    std::string argument;
-//};
-
 class cmd {
     public:
-    cmd() {}
-    cmd(const std::string exec, const std::vector<std::string> argList,
-        const std::string conn) : executable(exec), argumentList(argList),
-        connector(conn) {}
-
-    const std::string get_exec() { return executable; }
-
-    void set_exec(const std::string str) { executable=str; }
-
-    const std::string get_conn() { return connector; }
-
-    void set_conn(const std::string str) { connector=str; }
-
     private:
-    std::string executable;
-    std::vector<std::string> argumentList;
-    std::string connector;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 #include <unistd.h>
-#include <queue>
 #include "cmd.h"
 using namespace std;
 
-// also determines the number of commands to enqueue
-void parse(string line) {}
-
 int main(int argc, char **argv) {
-    queue<cmd> commands;
-    string line;
-    while (commands.empty()||commands.front().get_exec()!="exit") {
-        cout << "$ ";
-        cin >> line;
-        parse(line);
-    }
     return 0;
 }
