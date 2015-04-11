ff5629f384de7ac26e89b2e43bebdfc1a823b209
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-11 01:31:43 -0700

    added "parse" funtion into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <unistd.h>
+#include <queue>
 #include "cmd.h"
 using namespace std;
 
+void parse(const string &line, queue<cmd> &commands, char *s) {
+
+}
+
 int main(int argc, char **argv) {
+    queue<cmd> commands;
+    string line;
+    parse(line,commands,argv[0]);
     return 0;
 }
