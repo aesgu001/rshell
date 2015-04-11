f65ad2bb71d23ad70b20615022b85f4788cd9486
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-11 15:56:53 -0700

    modified "parse" function and members in cmd.h

--- src/cmd.h
+++ src/cmd.h
@@ -3,28 +3,22 @@
 
 class cmd {
     public:
-    cmd() : executable(""), argumentList(nullptr), connector("") {}
-    cmd(const std::string exec) : executable(exec) {}
-    cmd(const std::string exec, char **arList) : executable(exec) {
-        delete[] argumentList;
-        argumentList=arList;
-    }
-    cmd(const std::string exec, const std::string conn) : executable(exec),
+    cmd() : executable(nullptr), argumentlist(nullptr), connector("") {}
+    cmd(char *exec) : executable(exec) {}
+    cmd(char *exec, char **arList) : executable(exec),
+        argumentlist(arList) {}
+    cmd(char *exec, const std::string conn) : executable(exec),
         connector(conn) {}
-    cmd(const std::string exec, char **arList, const std::string conn) :
-    executable(exec), connector(conn) {
-        delete[] argumentList;
-        argumentList=arList;
-    }
-    ~cmd() { delete[] argumentList; }
+    cmd(char *exec, char **arList, const std::string conn) :
+        executable(exec), argumentlist(arList), connector(conn) {}
 
-    const std::string get_exec() { return executable; }
-    char **get_arList() { return argumentList; }
+    char *get_exec() { return executable; }
+    char **get_arList() { return argumentlist; }
     const std::string get_conn() { return connector; }
 
     private:
-    std::string executable;
-    char **argumentList;
+    char *executable;
+    char **argumentlist;
     std::string connector;
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 #include <queue>
 #include "cmd.h"
 using namespace std;
 
-void parse(const string &line, queue<cmd> &commands, char *s) {
+int nearest_connector(const string &s) {
+    if (s.find(";")==string::npos && s.find("&&")==string::npos &&
+        s.find("||")==string::npos) return -1;
+    return min(min(s.find("&&"),s.find(";")),min(s.find(";"),s.find("||")));
+}
 
+string get_nearest_connector(const string &s) {
+    int pos=nearest_connector(s);
+    if (pos==-1) return "";
+    if (s.at(pos)=='|') return "||";
+    else if (s.at(pos)=='&') return "&&";
+    return ";";
+}
+
+void parse(const string &line, queue<cmd> &commands) {
+    string conn=get_nearest_connector(line);
+    if (conn=="") { return; }
+    string l=line.substr(0,line.find(conn));
+    parse(line.substr(line.find(conn)+conn.size(),string::npos),commands);
 }
 
 int main(int argc, char **argv) {
     queue<cmd> commands;
     string line;
-    parse(line,commands,argv[0]);
+    getline(cin,line);
+    parse(line,commands);
     return 0;
 }
