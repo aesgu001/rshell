0d76ce6efe7fa6d1b05d184043cd310dccf39925
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-11 01:16:57 -0700

    added constructors/destructor for class cmd

--- src/cmd.h
+++ src/cmd.h
@@ -3,7 +3,25 @@
 
 class cmd {
     public:
+    cmd() : executable(""), argumentList(nullptr), connector("") {}
+    cmd(const std::string exec) : executable(exec) {}
+    cmd(const std::string exec, char **arList) : executable(exec) {
+        delete[] argumentList;
+        argumentList=arList;
+    }
+    cmd(const std::string exec, const std::string conn) : executable(exec),
+        connector(conn) {}
+    cmd(const std::string exec, char **arList, const std::string conn) :
+    executable(exec), connector(conn) {
+        delete[] argumentList;
+        argumentList=arList;
+    }
+    ~cmd() { delete[] argumentList; }
+
     private:
+    std::string executable;
+    char **argumentList;
+    std::string connector;
 };
 
 #endif
