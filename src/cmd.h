a39db67012aa6dca75604995d2db3f641a04a157
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-13 19:45:18 -0700

    added "print" function once more for class cmd

--- src/cmd.h
+++ src/cmd.h
@@ -39,6 +39,15 @@ class cmd {
         if (arlist_sz>arlist_cap) expand();
         *(argument_list+arlist_sz-1)=s;
     }
+
+    void print() const {
+        std::cout << "command: " << executable << std::endl;
+        std::cout << "arguments: ";
+        for (std::size_t i=0;i<arlist_sz;i++)
+            std::cout << *(argument_list+i) << " ";
+        std::cout << std::endl;
+        std::cout << "connector: " << connector<< std::endl;
+    }
 };
 
 #endif
