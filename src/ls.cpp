90a36ff857ba55cd43bf320b9114952ac1f13d71
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-04-25 15:41:13 -0700

    main function only reads all of current directory's contents

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
 using namespace std;
 
 int main(int argc, char **argv) {
+    DIR *dirp;
+    if (NULL==(dirp=opendir("."))) { perror("opendir"); exit(1); }
+    struct dirent *filespecs;
+    errno=0;
+    while (NULL!=(filespecs=readdir(dirp)))
+        cout<<filespecs->d_name<<"  ";
+    if (errno!=0) { perror("readdir"); exit(1); }
+    cout<<endl;
+    if (-1==closedir(dirp)) { perror("closedir"); exit(1); }
     return 0;
 }
