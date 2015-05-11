f2616f68010a6185a4404e8923eae361aae24757
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-05-10 22:39:35 -0700

    added data member std::string rdir in class 'cmd'

--- src/cmd.h
+++ src/cmd.h
@@ -9,17 +9,21 @@ class cmd {
     std::string exec;
     std::vector<std::string> arlist;
     std::string conn;
+    std::string rdir;
 
     public:
-    cmd(): exec(""), arlist(std::vector<std::string>()), conn("") {}
+    cmd(): exec(""), arlist(std::vector<std::string>()), conn(""),
+        rdir("") {}
 
     const std::string get_executable() const { return exec; }
     const std::vector<std::string> get_arlist() const { return arlist; }
     const std::string get_connector() const { return conn; }
+    const std::string get_redirection() const { return rdir; }
 
     void set_executable(const std::string &e) { exec=e; }
     void set_connector(const std::string &c) { conn=c; }
-    void push(const std::string &s) { arlist.push_back(s); }
+    void push(const std::string &arg) { arlist.push_back(arg); }
+    void set_redirection(const std::string &rd) { rdir=rd; }
 };
 
 #endif
