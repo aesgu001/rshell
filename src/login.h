b05dfd82efc7aa75b24a3a1869e76ffac849c0b3
Angelo Esguerra <aesgu001@hammer.cs.ucr.edu>
2015-05-29 21:59:27 -0700

    login does not throw an error anymore

--- src/login.h
+++ src/login.h
@@ -18,13 +18,16 @@ void login(const char *cwd) {
         perror("gethostname");
         exit(1);
     }
-    std::cerr<<user_host<<":~";
-    if (strcmp(cwd,getenv("HOME"))>0) {
+    std::cerr<<user_host<<":";
+    if (strlen(cwd)>=strlen(getenv("HOME"))) {
+        std::cerr<<"~";
         std::string home=getenv("HOME"),
             path=cwd;
         std::cerr<<path.substr(path.find(home)+home.length(),
             std::string::npos);
     }
+    else
+        std::cerr<<cwd;
     std::cerr<<"$ ";
 }
 
